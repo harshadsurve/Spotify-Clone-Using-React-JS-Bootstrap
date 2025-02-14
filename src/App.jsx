ef0830b145f4cc31d442ea3b7027ada8b2a3c873
harshad <harshadsurve2003@gmail.com>
2025-02-14 17:11:25 +0530

    `Updated README.md file`

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+        <Button variant="primary">Click Me</Button>
+
+    </>
+  )
+}
+
+export default App
