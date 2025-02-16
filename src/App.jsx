e84fb83fc6ff9affbbd1ab96303d6ab4520fe61a
harshad <harshadsurve2003@gmail.com>
2025-02-16 11:40:42 +0530

    spotify ui clone

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
+import React, { useContext } from 'react'
+import Player from './components/Player'
+import Sidebar from './components/Sidebar'
+import Display from './components/Display'
+import { PlayerContext } from './context/PlayerContext'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export const App = () => {
+  const {audioRef,track}= useContext(PlayerContext);
 
   return (
-    <>
-
-        <Button variant="primary">Click Me</Button>
-
-    </>
+    <div className='h-screen bg-black'>
+      <div className='h-[90%] flex'>
+          <Sidebar/>
+          <Display/>
+      </div>
+      <Player/>
+      <audio ref={audioRef} src={track.file} preload='auto'></audio>
+    </div>
   )
 }
-
-export default App
